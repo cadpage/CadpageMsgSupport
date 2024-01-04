5d5d1c0e84d39cc3249946db47dd98f00874281d
Ken Corbin <ken@cadpage.org>
2024-01-04 14:09:04 -0800

    New version fixes problems initiating phone calls under Android 14

--- app/src/main/java/net/anei/cadpagesupport/MainActivity.java
+++ app/src/main/java/net/anei/cadpagesupport/MainActivity.java
@@ -1,20 +1,25 @@
 package net.anei.cadpagesupport;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
-import androidx.annotation.NonNull;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+
 public class MainActivity extends Activity {
 
   private boolean needPhoneSupport;
-
+  private boolean suppressBatteryOptimization;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -24,7 +29,8 @@ public class MainActivity extends Activity {
     findViewById(R.id.req_button).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        checkPermissions(true);
+        if (checkPermissions(true))
+          checkBatteryOptimization(true);
       }
     });
 
@@ -41,6 +47,7 @@ public class MainActivity extends Activity {
 
     boolean launchedFromCadpage = intent.getBooleanExtra("net.anei.cadpage.LAUNCH", false);
     needPhoneSupport = intent.getBooleanExtra("net.anei.cadpage.CALL_PHONE", false);
+    suppressBatteryOptimization = intent.getBooleanExtra("new.anei.cadpage.SUPPRESS_BATTERY_OPT", false);
 
     // If we were not launched from Cadpage, do nothing except launch Cadpage and terminate
     // Cadpage will immediately turn around and ask us to set up the correct permissions
@@ -49,13 +56,22 @@ public class MainActivity extends Activity {
       finish();
     }
 
-    // If we were launched from Cadpage,
-    // our only purpose is to ensure that the required permissions have been granted
-    // If that is the case, we can quietly shut down.
-    if (checkPermissions(true)) {
-      Log.v("Cadpage message support enabled");
-      finish();
+    // We can be launched from cadpage for two seperate purposes.  The first will be to
+    // check if the necessary permission have been granted
+    boolean good;
+    if (!suppressBatteryOptimization) {
+      good = checkPermissions(true);
+      if (good) Log.v("Cadpage permissions OK");
     }
+
+    // The second is to confirm that battery optimization has been disabled
+    else {
+      good = checkBatteryOptimization(true);
+      if (good) Log.v("Cadpage battery optimization OK");
+    }
+
+    // If everything is kosher, shut everything down
+    if (good) finish();
   }
 
   private static final int PERM_REQ_ID = 3421;
@@ -71,7 +87,7 @@ public class MainActivity extends Activity {
   /**
    * See if all required permissions have been granted
    * @param request true if user should be asked to grant missing permissions
-   * @return true if they have, false otherwise
+   * @return true if everything is OK
    */
   private boolean checkPermissions(boolean request) {
 
@@ -95,11 +111,49 @@ public class MainActivity extends Activity {
     return false;
   }
 
+  /**
+   * Check if battery optimization has been disabled
+   * @param request true if user should be prompted to disable optimization
+   * @return true if everything is OK
+   */
+
+  private boolean checkBatteryOptimization(boolean request) {
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return true;
+
+    PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+    String packageName = getPackageName();
+    if (pm.isIgnoringBatteryOptimizations(getPackageName())) return true;
+
+    if (request) {
+      @SuppressLint({"InlinedApi", "BatteryLife"})
+      Intent newIntent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + packageName));
+      ContentQuery.dumpIntent(newIntent);
+      startActivity(newIntent);
+    }
+    return false;
+  }
+
   @Override
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    if (checkPermissions(false)) {
+
+    // If we were invoked to check permissions, see if that has been taken care of.  If so, we
+    // can shut things down
+    if (!suppressBatteryOptimization && checkPermissions(false)) {
       Log.v("Cadpage message support enabled");
+      finish();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    // If we were invoked to confirm battery optimization has been disabled, see if it has
+    // and if so we can shut down
+  if (suppressBatteryOptimization && checkBatteryOptimization(false)) {
+      Log.v("Cadpage battery optimization disabled");
       finish();
     }
   }
